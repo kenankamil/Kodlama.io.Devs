3ae84c18ce40e92a1710d5c1660607dce13b327c
EXT02D47162 <ozge.mutlu@consultant.turkcell.com.tr>
2022-09-06 14:50:53 +0300

    Update programming language endpoint, handler and profiles are added

---
+++ Application/Features/ProgrammingLanguages/Commands/UpdateProgrammingLanguage/UpdateProgrammingLanguageCommand.cs
@@ -0,0 +1,46 @@
+﻿using Application.Features.ProgrammingLanguages.Dtos;
+using Application.Features.ProgrammingLanguages.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.ProgrammingLanguages.Commands.UpdateProgrammingLanguage
+{
+    public class UpdateProgrammingLanguageCommand : IRequest<UpdateProgrammingLanguageDto>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public class UpdateProgrammingLanguageCommandHandler : IRequestHandler<UpdateProgrammingLanguageCommand, UpdateProgrammingLanguageDto>
+        {
+            private readonly IProgrammingLanguageRepository _programmingLanguageRepository;
+            private readonly IMapper _mapper;
+            private readonly ProgrammingLanguageBusinessRules _programmingLanguageBusinessRules;
+
+            public UpdateProgrammingLanguageCommandHandler(IProgrammingLanguageRepository programmingLanguageRepository, IMapper mapper, ProgrammingLanguageBusinessRules programmingLanguageBusinessRules)
+            {
+                _programmingLanguageRepository = programmingLanguageRepository;
+                _mapper = mapper;
+                _programmingLanguageBusinessRules = programmingLanguageBusinessRules;
+            }
+
+            public async Task<UpdateProgrammingLanguageDto> Handle(UpdateProgrammingLanguageCommand request, CancellationToken cancellationToken)
+            {
+                await _programmingLanguageBusinessRules.ProgrammingLanguageNameCanNotDublicatedWhenInsertedAsync(request.Name);
+
+                ProgrammingLanguage programmingLanguage = _mapper.Map<ProgrammingLanguage>(request);
+
+                ProgrammingLanguage result = await _programmingLanguageRepository.UpdateAsync(programmingLanguage);
+                UpdateProgrammingLanguageDto updateProgrammingLanguage = _mapper.Map<UpdateProgrammingLanguageDto>(result);
+
+                return updateProgrammingLanguage;
+            }
+        }
+    }
+}
